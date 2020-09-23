0bb1436a28b3375f507bd0ce6a89518455bb80ab
Gabriel Oliveira Rozendo <gabriel.rozendo@bancointer.com.br>
2020-09-23 02:39:54 -0300

    Add Fact model Add Category model

---
+++ ChuckNorrisFacts/ChuckNorrisFacts/Common/Models/FactCategory.swift
@@ -0,0 +1,18 @@
+//
+//  FactCategory.swift
+//  ChuckNorrisFacts
+//
+//  Created by Gabriel Oliveira Rozendo on 23/09/20.
+//  Copyright © 2020 Gabriel Oliveira Rozendo. All rights reserved.
+//
+
+import CoreData
+import Foundation
+
+// MARK: - CATEGORY
+
+typealias FactCategory = String
+
+extension FactCategory {
+    static var defaultCategory = "UNCATEGORIZED"
+}
